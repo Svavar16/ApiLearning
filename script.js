ecc7b88121f8f617dde9856a8e95737a6784d3f3
svavar16 <spg009@gmail.com>
2019-06-03 13:31:03 +0000

    getting the request and sending the movie title to the console

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+    // create a request and assign it to a variable
+    var request = new XMLHttpRequest();
+
+    // open a connection to https://ghibliapi.herokuapp.com and get the information
+    request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+    // accesing the data
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+
+        data.forEach(movie => {
+            // send the movie title into console log
+            console.log(movie.title);
+        });
+    }
+     // send the request
+    request.send();
 })
